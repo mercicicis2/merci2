ea30b7ebfaae7b82d3020ccb7e32dd92f5904aa4
mercicici <incmistum@gmail.com>
2024-05-21 23:30:38 -0300

    v0.5

--- src/freestyle.js
+++ src/freestyle.js
@@ -2,21 +2,28 @@ var caminhoimg;
 let temptestint = true;
 
 function mudaae(){
-    if(temptestint == true){
-    document.getElementById("slider").style.backgroundImage = "url('../img/wallpp2.jpg')"
+    if(temptestint == 1){
+    document.getElementById("slider").style.backgroundImage = "url('../img/bind.jpg')"
     console.log("obito") 
     }  
-    if(temptestint == false){
-    document.getElementById("slider").style.backgroundImage = "url('../img/wallpp1.png')" 
+    if(temptestint == 2){
+    document.getElementById("slider").style.backgroundImage = "url('../img/fracture.jpg')" 
     console.log("chrollo")
     }
+    if(temptestint == 3){
+        document.getElementById("slider").style.backgroundImage = "url('../img/icebox.jpg')" 
+        console.log("chrollo")
+        }
     
-    temptestint = !temptestint;
+    temptestint ++;
+    if(temptestint > 3) {
+        temptestint = 1;
+    }
 }
 
 
 
-var tmp = setInterval (mudaae, 3000);
+var tmp = setInterval (mudaae, 3500);
 
 console.log("hello world");
 
