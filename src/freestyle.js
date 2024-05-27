028c5daaae5b1ebc1bdba7b792df538ceabd8056
mercicici <incmistum@gmail.com>
2024-05-27 00:58:09 -0300

    aesgit

--- src/freestyle.js
+++ src/freestyle.js
@@ -4,19 +4,24 @@ let temptestint = true;
 function mudaae(){
     if(temptestint == 1){
         document.getElementById("slider").setAttribute("src","../img/bind.jpg");
+        document.getElementById("parceirosgameicon").setAttribute("src","../img/othericons/valorant.jpg");
    
     console.log("obito") 
     }  
+
     if(temptestint == 2){
         document.getElementById("slider").setAttribute("src","../img/fracture.jpg");
+        document.getElementById("parceirosgameicon").setAttribute("src","../img/othericons/lol.png");
     
     console.log("chrollo")
     }
+
     if(temptestint == 3){
         document.getElementById("slider").setAttribute("src","../img/icebox.jpg");
+        document.getElementById("parceirosgameicon").setAttribute("src","../img/othericons/cs2.png");
         
-        console.log("chrollo")
-        }
+    console.log("chrollo")
+    }
     
     temptestint ++;
     if(temptestint > 3) {
